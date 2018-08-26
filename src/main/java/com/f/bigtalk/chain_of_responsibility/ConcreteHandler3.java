02438ac07dc7c1e547b39a65f3c1a8230bed0eca
f <839322915@qq.com>
2018-08-26 21:09:57 +0800

    add chain-of-responsibility

---
+++ src/main/java/com/f/bigtalk/chain_of_responsibility/ConcreteHandler3.java
@@ -0,0 +1,12 @@
+package com.f.bigtalk.chain_of_responsibility;
+
+public class ConcreteHandler3 extends Handler {
+    @Override
+    public void handlerRequest(int request) {
+        if (request > 20 && request < 30) {
+            System.out.println("handler request3");
+        } else if (handler != null) {
+            handler.handlerRequest(request);
+        }
+    }
+}

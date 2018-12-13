aa41f4075d279416d4530b36069b4dbe66b333f4
f <839322915@qq.com>
2018-12-13 23:51:53 +0800

    Add synchronized test

--- src/main/java/com/f/java/concurrent/thread/lock/SynchronizedEventGenerator.java
+++ src/main/java/com/f/java/concurrent/thread/lock/SynchronizedEventGenerator.java
@@ -2,27 +2,18 @@ package com.f.java.concurrent.thread.lock;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-public class MutexEventGenerator extends IntGenerator {
+public class SynchronizedEventGenerator extends IntGenerator {
     private int currentEventValue = 0;
-    private Lock lock = new ReentrantLock();
 
     @Override
     public synchronized int next() {
-        lock.lock();
-        try {
-            ++currentEventValue;
-            System.out.println("first + 1 " + currentEventValue);
-            Thread.yield();
-            ++currentEventValue;
-            System.out.println("second + 1 " + currentEventValue);
-            lock.unlock();
-        } finally {
-            return currentEventValue;
-
-        }
+        ++currentEventValue;
+        System.out.println("first + 1 " +currentEventValue);
+        Thread.yield();
+        ++currentEventValue;
+        System.out.println("second + 1 "  + currentEventValue);
+        return currentEventValue;
     }
 
     public static void main(String[] args) throws InterruptedException {

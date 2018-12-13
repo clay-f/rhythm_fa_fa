2f84507d116c0ebe1892f0a63100989cce66222f
f <839322915@qq.com>
2018-12-13 23:49:37 +0800

    Add AtomInteger test

---
+++ src/main/java/com/f/java/concurrent/atom/AtomicIntegerTest.java
@@ -0,0 +1,48 @@
+package com.f.java.concurrent.atom;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicIntegerTest implements Runnable {
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private int evenIncrement() {
+        return atomicInteger.addAndGet(2);
+    }
+
+    public int getAtomicInteger() {
+        return atomicInteger.get();
+    }
+
+    @Override
+    public void run() {
+        while (true)
+            evenIncrement();
+    }
+
+    public static void main(String[] args) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.err.println("Aborting");
+                System.exit(0);
+            }
+        }, 5000);
+
+
+        var exec = Executors.newFixedThreadPool(10);
+        AtomicIntegerTest atomicIntegerTest = new AtomicIntegerTest();
+        exec.execute(atomicIntegerTest);
+        while (true) {
+            int val = atomicIntegerTest.getAtomicInteger();
+            if (val % 2 != 0){
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+
+    }
+
+}

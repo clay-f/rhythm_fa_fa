f0233622b15abed965c14e2e6ecaf51acb3a1fff
f <839322915@qq.com>
2018-12-31 11:59:15 +0800

    Delete extract file

--- src/main/java/com/f/studyDesignPatterns/interfacePattern/bridge/HskMachineManager2.java
+++
@@ -1,4 +0,0 @@
-package com.f.studyDesignPatterns.interfacePattern.bridge;
-
-public class HskMachineManager2 extends MachineManager2 {
-}
--- src/main/java/com/f/studyDesignPatterns/interfacePattern/bridge/MachineManager2.java
+++
@@ -1,9 +0,0 @@
-package com.f.studyDesignPatterns.interfacePattern.bridge;
-
-public class MachineManager2 {
-  private  MachineDriver machineDriver;
-
-  void shutdown() {
-
-  }
-}

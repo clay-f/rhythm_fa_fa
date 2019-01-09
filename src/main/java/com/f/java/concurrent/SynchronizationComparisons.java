556fc6f0be1951ea2469dea089cdc3a2728e70f4
f <839322915@qq.com>
2019-01-10 00:25:53 +0800

    Update thread description

--- src/main/java/com/f/java/concurrent/SynchronizationComparisons.java
+++ src/main/java/com/f/java/concurrent/SynchronizationComparisons.java
@@ -77,8 +77,7 @@ abstract class Accumulator {
         printf("%-13s: %13d\n", id, duration);
     }
 
-    public static void
-    report(Accumulator acc1, Accumulator acc2) {
+    public static void report(Accumulator acc1, Accumulator acc2) {
         printf("%-22s: %.2f\n", acc1.id + "/" + acc2.id,
                 (double) acc1.duration / (double) acc2.duration);
     }

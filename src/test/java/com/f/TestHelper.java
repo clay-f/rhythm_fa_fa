c42f4ddef9fbccc3dfcf82bc1f4a89d795c333f3
f <839322915@qq.com>
2020-03-22 22:05:18 +0800

    add TestHelper

---
+++ src/test/java/com/f/TestHelper.java
@@ -0,0 +1,8 @@
+package com.f;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class TestHelper {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+}

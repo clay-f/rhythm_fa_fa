03370a43a057f76c9b742dec32c176ceba8c5b2c
f <839322915@qq.com>
2020-03-22 22:06:15 +0800

    add test jvm memory area

--- src/main/java/com/f/jvm/memory/JavaVMStackSOF.java
+++ src/main/java/com/f/jvm/memory/JavaVMStackSOF.java
@@ -1,22 +1,25 @@
 package com.f.jvm.memory;
 
+
 /**
- *VM Args:-Xss128k
+ * VM Args：-Xss144k
+ * 设置栈容量，让 jvm 因栈容量太小而抛出异常.
  */
-public class JavaVMStackSOF{
-    private int stackLength=1;
-    public void stackLeak(){
+public class JavaVMStackSOF {
+
+    private int stackLength = 1;
+
+    public void stackLeak() {
         stackLength++;
         stackLeak();
     }
-    
-    public static void main(String[]args)throws Throwable{
-        JavaVMStackSOF oom=new JavaVMStackSOF();
-        assert 0 > 1;
-        try{
+
+    public static void main(String[] args) throws Throwable {
+        JavaVMStackSOF oom = new JavaVMStackSOF();
+        try {
             oom.stackLeak();
-        }catch(Throwable e){
-            System.out.println("stack length:"+oom.stackLength);
+        } catch (Throwable e) {
+            System.out.println("stack length:" + oom.stackLength);
             throw e;
         }
     }

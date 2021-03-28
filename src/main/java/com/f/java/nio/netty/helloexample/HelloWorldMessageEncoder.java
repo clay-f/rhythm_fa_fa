06aeb6d83061b103675c686b67074e850ba00559
f <839322915@qq.com>
2021-03-28 15:04:19 +0800

    remove gradle replace maven

--- src/main/java/com/f/java/nio/netty/helloexample/HelloWorldMessageEncoder.java
+++ src/main/java/com/f/java/nio/netty/helloexample/HelloWorldMessageEncoder.java
@@ -3,9 +3,6 @@ package com.f.java.nio.netty.helloexample;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
-import io.netty.handler.codec.string.LineEncoder;
-import io.netty.handler.codec.string.LineSeparator;
-import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +12,5 @@ public class HelloWorldMessageEncoder extends MessageToByteEncoder<String> {
     @Override
     protected void encode(ChannelHandlerContext ctx, String msg, ByteBuf out) throws Exception {
         logger.info("class name: {}, 对 msg: {} 进行 encode", HelloWorldMessageEncoder.class.getSimpleName(), msg);
-        new LineEncoder(LineSeparator.DEFAULT);
-        out.writeCharSequence(msg, CharsetUtil.UTF_8);
     }
 }
--- src/main/java/com/f/jvm/memory/RuntimeConstantPoolOOM.java
+++ src/main/java/com/f/jvm/memory/RuntimeConstantPoolOOM.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class RuntimeConstantPoolOOM{
     public static void main(String[]args){
         //使用List保持着常量池引用, 避免Full GC回收常量池行为
-        List<String> list=new ArrayList<String>();
+        List<String> list=new ArrayList<>();
         //10MB的PermSize在integer范围内足够产生OOM了
         int i=0;
         while(true){
